446d15e7787ce557da88f5cfea726bf4a1d0027f
Pau Peinado <paupeinado@gmail.com>
2015-04-28 18:04:27 +0200

    Removes angular deferred bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -25,9 +25,6 @@ require.config({
 	
 });
 
-// http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
-window.name = "NG_DEFER_BOOTSTRAP!";
-
 require(['app'], function(app) {
 
 	app.run();
