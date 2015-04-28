83e73077b617f521dda1f35b3b46a44de1205faa
Pau Peinado <paupeinado@gmail.com>
2015-04-28 16:00:18 +0200

    Removes console.log

--- src/scripts/directives/_loader.js
+++ src/scripts/directives/_loader.js
@@ -7,7 +7,6 @@ function(angular) {
 	// Define directives
 	for(var i= 1; i< arguments.length; i++) {
 
-		console.log(arguments[i]);
 		directives.directive(arguments[i].name, arguments[i].directive);
 	}
 
